a71519894c42233a35a0d476eeb8e2b18c19f7fc
Gryfentube <gryfentube@gmail.com>
2018-06-09 20:46:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,7 +9,7 @@ db.defaults({ ann:[]})
     .write()
 var bot = new Discord.Client();
 var prefix = ("_");
-var activ = ("créer un monde sans limite"); //
+var activ = ("créer un monde sans limite"); //modifier la valeur entre guillemets pour changer son état au démarage
 var values = ("empty");
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: activ}});
@@ -27,22 +27,25 @@ bot.on('message', message => {
 
     if (!message.content.startsWith(prefix)) return;
     var args = message.content.substring(prefix.length).split(" ");
-
+    message.channel.send(author);
     switch (args[0].toLowerCase()){
 
         case "say":
-        var author = message.member.displayName;
-        var value = message.content.substr(5);
+            var author = message.member.displayName;
+            var value = message.content.substr(5);
             if ((message.member.id === "239310906981482496") || (message.member.id === "187554016853622784")){
-        message.reply("Le message a été envoyé :D"); //respond
-        bot.channels.get('452800422655033365').send(value); //annonce
-        bot.channels.get('455070342612910081').send("L'annonce " + value + " a été envoyé par " + author); //console
+                message.reply("Le message a été envoyé :D"); //respond
+                bot.channels.get('452800422655033365').send(value); //annonce
+                bot.channels.get('455070342612910081').send("L'annonce " + value + " a été envoyé par " + author); //console
             }
             else {
                 message.reply("tu ne peux pas faire ça"); //respond
                 bot.channels.get('455070342612910081').send(author + "a essayé de faire l'annonce" + value); //console
             }
-        break;
+break;
+        case "commu":
+            if
+break;
         //message.reply("```Bonjour je me présente je suis un bot```");
         case "parler":
         var author = message.member.displayName;
@@ -58,7 +61,6 @@ bot.on('message', message => {
         var author = "oui";
         var author = message.member.displayName;
         console.log(author);
-        message.channel.send(author);
         break;
     }
 
