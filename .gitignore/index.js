835ec2554e8edc58de19938c310ac10f57170a8d
Gryfentube <gryfentube@gmail.com>
2018-06-12 00:40:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -48,8 +48,8 @@ bot.on('message', message => {
                                                                icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}}))};
 
     if (message.channel.id === annPoDXSIY){
-        bot.channels.get(consauleDXSIY).sendMessage({embed: {color: 0x202020, fields: [{name: "ANONCE", value: value}]}}) //annonce
-            .then(bot.channels.get(consauleDXSIY).sendMessage({embed: {color: 0x202020, author: {name: "Nouvelle annonce envoyé par" + author,
+        bot.channels.get(annDXSIY).sendMessage({embed: {color: 0x202020, fields: [{name: "ANNONCE", value: value}]}}) //annonce
+            .then(bot.channels.get(consauleDXSIY).sendMessage({embed: {color: 0x202020, author: {name: "Nouvelle annonce envoyé par " + author,
                                                                icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"},
                                                                fields: [{name: "L'annonce est :", value: value}]}}))}; //console
     if (!message.content.startsWith(prefix)) return;
