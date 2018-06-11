bff8fe1c1097f8370b19b27e7ed5a7096184ec54
Gryfentube <gryfentube@gmail.com>
2018-06-11 23:41:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -47,7 +47,8 @@ bot.on('message', message => {
     if (message.channel.id === activitDXSIY) {
     var value = message.content;
     bot.user.setPresence({ game: { name: value}})
-        .then
+        .then(bot.channels.get(consauleDXS).sendMessage({embed: {color: 0x202020, author: {name: "Je joue maintenant à " + value + " grâce à " + author,
+      icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}}))};
     }
     
     if (!message.content.startsWith(prefix)) return;
