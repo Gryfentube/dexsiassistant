1242b0e3b906be1c06c8923ca74001586c891f94
Gryfentube <gryfentube@gmail.com>
2018-07-31 17:36:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -101,7 +101,6 @@ bot.on('message', message => {
                                                                icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}}))};
     if (message.content === "listemojis") {
   const emojiList = message.guild.emojis.map(e=>e.toString()).join("/n");
-  const emojiList = message.guild.emojis.map(e=>e.toString()).join("/n");
   message.channel.sendMessage(emojiList);
   message.channel.sendMessage("`" + emojiList + "`");
     }
